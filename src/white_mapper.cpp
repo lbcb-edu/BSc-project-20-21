ec6b98b7cd7538e149b0580ad8ccd76d7f2662a2
Roko Grbelja <roko.grbelja@fer.hr>
2020-11-01 20:15:45 +0100

    Pocetak definicije funkcionalnosti white_mappera

--- src/white_mapper.cpp
+++ src/white_mapper.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <getopt.h>
 #include "projectControl.h"
+#include "../bioparser/include/bioparser/fasta_parser.hpp"
+#include "../bioparser/include/bioparser/fastq_parser.hpp"
 
 static int version_req;
 static int help_req;
@@ -8,6 +10,37 @@ static int help_req;
 void version_print(){
     std::cout << PROJECT_VER << std::endl;
 }
+
+//Checks if passed arguments are in fasta and fastq formats
+bool checkArgs() {
+	std::string extension_fasta[5] = [".fasta", ".fna", ".ffn", ".faa", ".frn"];
+	char* p = strrchr(argv[0], '.');
+	int found = 0;
+	if (p) {
+		for (int i = 0; i < 5; i++) {
+			if (strcmp(p, extension[i]) {
+				found++;
+					break;
+			}
+		}
+	}
+
+	if (found != 1) {
+		return false;
+	}
+
+	std::string extension_fastq[7] = [".fasta", ".fna", ".ffn", ".faa", ".frn", ".fastq", ".fq"];
+	p = strrchr(argv[1], '.');
+	if (p) {
+		for (int i = 0; i < 7; i++) {
+			if (strcmp(p, extension[i]) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+	
 
 void help_print(){
     std::cout << "\n" PROJECT_NAME " usage:\n"
@@ -35,7 +68,7 @@ int main(int argc, char *argv[]){
             {0, 0, 0, 0}    
     };
     
-    while((i = getopt_long(argc, argv, "hv", long_options, nullptr)) != -1){
+    while ((i = getopt_long(argc, argv, "hv", long_options, nullptr)) != -1){
         
         switch (i)
         {
@@ -58,11 +91,33 @@ int main(int argc, char *argv[]){
         }
     }
 
-    if(version_req){
+    if (version_req){
         version_print();
     } else if(help_req){
         help_print();
     }
 
-    return 0;
+	if (argc != 2) {
+		std::cerr << "error: invalid input, please include exactly two files";
+		return 1;
+	}
+
+	if (!checkArgs()) {
+		std::cerr << "error: invalid file format, please pass two files in FASTA and FASTQ formats in that order"
+			return 1;
+	}
+
+	struct Sequence {  // or any other name
+	public:
+		Sequence(  // required arguments
+			const char*, std::uint32_t,
+			const char*, std::uint32_t) {
+			// implementation
+		}
+	}
+	auto p = bioparser::Parser<Sequence>::Create<bioparser::FastaParser>(path);
+
+	// parse whole file
+	auto s = p->Parse(-1);
+	return 0;
 }
