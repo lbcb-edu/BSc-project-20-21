446a3a7bdb9a4cfb89cb212bebd2c31a0dd6e1c3
Jaster111 <ante.grgurevic@gmail.com>
2020-11-03 00:01:37 +0100

    razradena funkcionalnost parsera

--- src/white_mapper.cpp
+++ src/white_mapper.cpp
@@ -13,7 +13,7 @@ void version_print(char *argv[]){
 //Checks if passed arguments are in fasta and fastq formats
 bool checkArgs(char *argv[]) {
 	const char* extension_fasta[5] = { ".fasta", ".fna", ".ffn", ".faa", ".frn" };
-	char* p = strrchr(argv[0], '.');
+	char* p = strrchr(argv[1], '.');
 	int found = 0;
 	if (p) {
 		for (int i = 0; i < 5; i++) {
@@ -29,7 +29,7 @@ bool checkArgs(char *argv[]) {
 	}
 
 	const char* extension_fastq[7] = {".fasta", ".fna", ".ffn", ".faa", ".frn", ".fastq", ".fq"};
-	p = strrchr(argv[1], '.');
+	p = strrchr(argv[2], '.');
 	if (p) {
 		for (int i = 0; i < 7; i++) {
 			if (strcmp(p, extension_fastq[i])) {
@@ -98,7 +98,7 @@ int main(int argc, char *argv[]){
 		return 0;
     }
 
-	if (argc != 2) {
+	if (argc != 3) {
 		std::cerr << "error: invalid input, please include exactly two files\n";
 		return 1;
 	}
@@ -111,16 +111,61 @@ int main(int argc, char *argv[]){
 	struct Sequence {  // or any other name
 	public:
 		Sequence(  // required arguments
-			const char*, std::uint32_t,
-			const char*, std::uint32_t) {
-			// implementation
+			const char* name, std::uint32_t nameLength,
+			const char* data, std::uint32_t dataLength) {
+			this->name = name;
+			this->data = data;
+			this->dataLength = dataLength;
+			this->nameLength = nameLength;
 		}
+
+		std::string getName() {
+			return this->name;
+		}
+
+		std::string getData() {
+			return this->data;
+		}
+
+		std::uint32_t getNameLength() {
+			return this->nameLength;
+		}
+
+		std::uint32_t getDataLength() {
+			return this->dataLength;
+		}
+
+	private:
+		const char* name;
+		const char* data;
+		std::uint32_t nameLength;
+		std::uint32_t dataLength;
 	};
 
 
-	auto seq = bioparser::Parser<Sequence>::Create<bioparser::FastaParser>(argv[0]);
+	auto genome = bioparser::Parser<Sequence>::Create<bioparser::FastaParser>(argv[1]);
+	auto g = genome->Parse(-1);
+	int g_size = (int)g.size();
 
-	// parse whole file
-	auto s = seq->Parse(-1);
+	auto fragments = bioparser::Parser<Sequence>::Create<bioparser::FastaParser>(argv[2]);
+	auto f = fragments->Parse(-1);
+	int f_size = (int)f.size();
+
+	int sum = 0;
+	//names of sequences in the reference file and their lengths
+	for (int i = 0; i < g_size; i++) {
+		std::cerr << "Name of sequence: " << g[i]->getName() << "\n" << "Length of sequence: " << g[i]->getDataLength() << "\n\n";
+	}
+
+	//number of sequences in the fragments file
+	for (int i = 0; i < f_size; i++) {
+		sum += f[i]->getDataLength();
+	}
+	std::cerr << "Number of sequences: " << sum << "\n\n";
+
+	float avg_size = sum / f_size;
+	std::cerr << "Average length of fragments: " << avg_size << "\n\n";
+
+
 	return 0;
 }
