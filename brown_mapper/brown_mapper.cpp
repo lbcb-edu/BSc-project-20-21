37d4f89c24a828a9d53feecc69f6277471ad22b0
zvonimirbrisevac <zb51687@fer.hr>
2020-11-11 02:01:27 +0100

    Obrađuje prvi fasta file

--- brown_mapper/brown_mapper.cpp
+++ brown_mapper/brown_mapper.cpp
@@ -4,7 +4,7 @@
 #include "bioparser/include/bioparser/fastq_parser.hpp"
 #include "bioparser/include/bioparser/fasta_parser.hpp"
 
-#define VERSION "v0.1";
+#define VERSION "v0.1"
 
 static option long_options[] = {
     {"help", no_argument, nullptr, 'h'},
@@ -46,19 +46,20 @@ for (auto t = p->parse(chunk_size); !t.empty(); t = p->parse(chunk_size)) {
 //fasta
 struct Sequence {  // or any other name
   public:
-    const char* sequenceName;
-    std::uint32_t SequenceNameLength;
-    const char* sequenceSequence;
-    std::uint32_t sequenceSequenceLength;
-    const char* sequenceQuality;
-    std::uint32_t sequenceQualityLength;
+    std::string sequenceName;
+    std::string sequenceSequence;   
+    std::string sequenceQuality;
 
     Sequence(  // required arguments
         const char* name, std::uint32_t nameLength,
         const char* sequence, std::uint32_t sequenceLength,
         const char* quality = nullptr, std::uint32_t qualityLength = 0) :
-            sequenceName(name), SequenceNameLength(nameLength),
-            sequenceSequence(sequence)  {};
+            sequenceName(name, nameLength),
+            sequenceSequence(sequence, sequenceLength) {
+                if (quality != nullptr) {
+                    sequenceQuality = std::string(quality, qualityLength);
+                }
+            }
 };
 //auto p = bioparser::Parser<Sequence>::Create<bioparser::FastaParser>(path);
 
@@ -71,32 +72,48 @@ struct Sequence {  // or any other name
 int main(int argc, char* argv[]) {
 
 
-    std::cout << "evo me" << std::endl;
+    //std::cout << "evo me1" << std::endl;
     int c = getopt_long(argc, argv, "hv", long_options, 0);
     if (c != -1) {
         switch (c){
             case 'h' :
                 std::cout << help << std::endl;
-                break;
+                return 0;;
             case 'v' :
-                std::cout << VERSION;
-                break;
+                std::cout << VERSION << std::endl;
+                return 0;
             default:
-                break;
+                return 0;
         }
     }
-
+    //std:: cout << "evo me 2" << std::endl;
     if (argc == 2) { //promjeni u 3
+        //std::cout << "ide pravit stringove";
         std::string file1 = argv[1];
-        std::string file2 = argv[2];
+        //std::string file2 = argv[2];
         if (file1.compare(file1.length() - 6, 6, ".fasta") == 0 ||
             file1.compare(file1.length() - 3, 3, ".fa") == 0) {
                 auto p = bioparser::Parser<Sequence>::Create<bioparser::FastaParser>(argv[1]);
                 std::vector<std::unique_ptr<Sequence>> s = p->Parse(-1);
+                std::cout << s.front()->sequenceName << std::endl;
+                std::cout << s.front()->sequenceSequence.length() << std::endl;
                 
-                std::cout << s.at(0)->sequenceName << std::endl;
-                std::cout << s.at(0)->sequenceSequence << std::endl;
+        } else {
+            std::cerr << "First file needs to be in FASTA format!" << std::endl;
         }
+
+        /*if ((file2.compare(file2.length() - 6, 6, ".fastq") == 0 ||
+            file2.compare(file2.length() - 3, 3, ".fq") == 0)) {
+                auto p = bioparser::Parser<Sequence>::Create<bioparser::FastqParser>(argv[2]);
+                std::vector<std::unique_ptr<Sequence>> s;
+                std::uint32_t chunk_size = 500 * 1024 * 1024;  // 500 MB
+                for (auto t = p->Parse(chunk_size); !t.empty(); t = p->Parse(chunk_size)) {
+                     s.insert(
+                        s.end(),
+                        std::make_move_iterator(t.begin()),
+                        std::make_move_iterator(t.end()));
+                }
+        }*/
 
     }
 
