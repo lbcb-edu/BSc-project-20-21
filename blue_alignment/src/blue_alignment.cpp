8ec4afa78357b460b2dfcbe84b47f5ded11a15be
marin-rasic <mrasic123@gmail.com>
2020-11-11 23:39:54 +0100

    Added cigar creator

--- blue_alignment/src/blue_alignment.cpp
+++ blue_alignment/src/blue_alignment.cpp
@@ -42,6 +42,51 @@ class Aligner {
     matrix_[0][0] = {0, Operation::kNone};
   }
 
+
+
+  std::string CigarCreator(int& i, int& j, std::string initial_cigar) {
+    while (matrix_[i][j].operation != kNone) {
+      switch(matrix_[i][j].operation) {
+        case kMatch:
+          initial_cigar = "=" + initial_cigar;
+          i--;
+          j--;
+          break;
+        case kMismatch:
+          initial_cigar = "X" + initial_cigar;
+          i--;
+          j--;
+          break;
+        case kDelete:
+          initial_cigar = "D" + initial_cigar;
+          i--;
+          break;
+        case kInsert:
+          initial_cigar = "I" + initial_cigar;
+          j--;
+          break;
+        }
+    }
+
+    std::string cigar_result = "";
+    char letter = initial_cigar[0];
+    int count = 1;
+    for (int i = 1; i < initial_cigar.length(); i++) {
+      if (initial_cigar[i] == letter) {
+        count++;
+      } else {
+        cigar_result += std::to_string(count) + letter;
+        count = 1;
+        letter = initial_cigar[i];
+      }
+    }
+    cigar_result += std::to_string(count) + letter;
+
+    return cigar_result;
+  }
+
+
+
   int NeedlemanWunsch() {
     for (int i = 1; i < query_len + 1; i++)
       matrix_[i][0] = {gap * i, Operation::kDelete};
@@ -65,9 +110,19 @@ class Aligner {
         });
       }
     }
-    // TODO: cigar
+    
+    if (cigar) {
+      std::string initial_cigar = "";
+      int i = query_len;
+      int j = target_len;
+      *cigar = CigarCreator(i,j,initial_cigar);
+      *target_begin = 1;
+    }
+
     return matrix_[query_len][target_len].value;
   }
+
+
 
   int SmithWaterman() {
     for (int i = 1; i < query_len + 1; i++)
@@ -77,6 +132,8 @@ class Aligner {
 
     Cell neutral = {0,Operation::kNone};
     int max_value = 0;
+    int max_row_index = 0;
+    int max_column_index = 0;
 
     for (int i = 1; i < query_len + 1; i++) {
       for (int j = 1; j < target_len + 1; j++) {
@@ -91,18 +148,37 @@ class Aligner {
         Cell left = {matrix_[i][j - 1].value + gap, Operation::kInsert};
 
 
-        matrix_[i][j] = std::max({neutral, diagonal, top, left}, [](Cell a, Cell b) {
+        matrix_[i][j] = std::max({diagonal, top, left, neutral}, [](Cell a, Cell b) {
           return a.value < b.value;
         });
 
         if (matrix_[i][j].value > max_value) {
             max_value = matrix_[i][j].value;
+            max_row_index = i;
+            max_column_index = j;
         } 
       }
     }
-    // TODO: cigar
+
+    if (cigar) {
+      std::string initial_cigar = "";
+      int i = max_row_index;
+      int j = max_column_index;
+      for (int k = i+1; k < query_len + 1; k++) {
+        initial_cigar = "S" + initial_cigar;
+      }
+      std::string cigar_result = CigarCreator(i,j,initial_cigar);
+      if (i != 0) {
+        cigar_result = std::to_string(i) + "S" + cigar_result;
+      }
+      *target_begin = j+1;
+      *cigar = cigar_result;
+    }
+    
     return max_value;
   }
+
+
 
   int SemiGlobal() { 
     for (int i = 1; i < query_len + 1; i++)
@@ -130,24 +206,41 @@ class Aligner {
       }
     }
 
-    int max_value_row = 0;
-    int max_value_column = 0;
-    int max_value;
+    int max_row_index = 0;
+    int max_column_index = 0;
+
+    int max_value = matrix_[0][target_len].value;
 
     for (int i = 0; i < query_len + 1; i++) {
-        if (matrix_[i][target_len].value > max_value_column) {
-          max_value_column = matrix_[i][target_len].value;
+        if (matrix_[i][target_len].value > max_value) {
+          max_value = matrix_[i][target_len].value;
+          max_row_index = i;
+          max_column_index = target_len;
         }
     }
     for (int j = 0; j < target_len + 1; j++) {
-        if (matrix_[query_len][j].value > max_value_row) {
-          max_value_row = matrix_[query_len][j].value;
+        if (matrix_[query_len][j].value > max_value) {
+          max_value = matrix_[query_len][j].value;
+          max_row_index = query_len;
+          max_column_index = j;
         }
     }
 
-    max_value = max_value_column > max_value_row ? max_value_column : max_value_row;
+    if (cigar) {
+      std::string initial_cigar = "";
+      int i = max_row_index;
+      int j = max_column_index;
+      for (int k = i+1; k < query_len + 1; k++) {
+        initial_cigar = "S" + initial_cigar;
+      }
+      std::string cigar_result = CigarCreator(i,j,initial_cigar);
+      if (i != 0) {
+        cigar_result = std::to_string(i) + "S" + cigar_result;
+      }
+      *target_begin = j+1;
+      *cigar = cigar_result;
+    }
 
-    // TODO: cigar
     return max_value;
   } 
 
