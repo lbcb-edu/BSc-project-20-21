6873e9acc6ef86b362bc7b146d4b388dc238a683
MatijaHudin <matija.hudin@yahoo.com>
2020-11-14 17:56:04 +0100

    Napravljeni local i global, tako nam bog pomogao

--- brown_mapper/brown_alignment.cpp
+++ brown_mapper/brown_alignment.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "brown_alignment.hpp"
-
+#include <algorithm>
+#include <math.h>
 namespace brown {
 
     //enum AlignmentType {GLOBAL, LOCAL, SEMIGLOBAL};
@@ -18,6 +19,50 @@ namespace brown {
         int gap,
         std::string* cigar = nullptr,
         unsigned int* target_begin = nullptr) {
-            return 2;
+            
+            if(type == GLOBAL) {
+                int m[query_len + 1][target_len + 1];
+                m[0][0]=0;
+                for (int i = 1; i < query_len + 1; i++) {
+                    m[i][0]=i * gap;
+                }
+                for (int j = 1; j < target_len + 1; j++) {
+                    m[0][j]=j * gap;
+                }
+                for (int i = 1; i < query_len+1; i++)
+                    for (int j = 1; j < target_len+1; j++) {
+                        int matchCost;
+                        if (query[i] == target[j]) matchCost=m[i-1][j-1] + match;
+                        else matchCost=m[i-1][j-1] + mismatch;
+                        m[i][j]=std::max(std::max(matchCost, m[i][j-1] + gap), m[i-1][j] + gap);
+                    }
+                return m[query_len][target_len];
+
+            }
+            else if (type == LOCAL) {
+                int maxCell = 0;
+                int m[query_len + 1][target_len + 1];
+                m[0][0] = 0;
+                for (int i = 1; i < query_len + 1; i++) {
+                    m[i][0] = 0;
+                }
+                for (int j = 1; j < target_len + 1; j++) {
+                    m[0][j] = 0;
+                }
+                for (int i = 1; i < query_len + 1; i++)
+                    for (int j = 1; j < target_len + 1; j++) {
+                        int matchCost;
+                        if (query[i]==target[j]) matchCost = m[i-1][j-1]+match;
+                        else matchCost=m[i-1][j-1] + mismatch;
+                        m[i][j]=std::max(std::max(0, matchCost), std::max(m[i][j-1] + gap, m[i-1][j] + gap));
+                        maxCell=std::max(maxCell, m[i][j]);
+                    }
+                    return maxCell;
+            }
+            else if (type == SEMIGLOBAL){
+                //TODO
+            }   
+            else std::cerr << "Invalid AlignmentType" << std::endl;
+            exit(1);
     }
 }
