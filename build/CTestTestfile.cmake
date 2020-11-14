7026d626070fd48cc7e4498f1d32e117e0166fb0
Lucija <lucija.strejcek@fer.hr>
2020-11-14 18:16:30 +0100

    Update CMakeLists

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andrija/Desktop/pink_mapper/src
-# Build directory: /home/andrija/Desktop/pink_mapper/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(UnitTests "pink_alignment_test")
-set_tests_properties(UnitTests PROPERTIES  _BACKTRACE_TRIPLES "/home/andrija/Desktop/pink_mapper/src/CMakeLists.txt;49;add_test;/home/andrija/Desktop/pink_mapper/src/CMakeLists.txt;0;")
--- build/Pink_mapperConfig.h
+++
@@ -1,4 +0,0 @@
-// the configured options and settings for Tutorial
-#define pink_mapper_VERSION_MAJOR 0
-#define pink_mapper_VERSION_MINOR 2
-#define pink_mapper_VERSION_PATCH 0
