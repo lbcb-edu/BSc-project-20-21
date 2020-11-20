b77e63a6fa96a403d53016adee5f539213d666a7
Leonard.Inkret@avl.com <Leonard.Inkret@avl.com>
2020-11-20 12:35:20 +0100

    add SemiGlobal tests

--- test/blonde_alignment_test.cpp
+++ test/blonde_alignment_test.cpp
@@ -23,11 +23,19 @@ void testAlignmentScenario(
 }
 
 TEST(BlondeAlignmentTests, Global) {
-    testAlignmentScenario("GCATGCU", "GATTACA", blonde::alignment::AlignmentType::kGlobal, 0, "1=1D1=1I1=1X1=1X", 0);
-    testAlignmentScenario("AGTCTTTGAT", "GATTAGA", blonde::alignment::AlignmentType::kGlobal, 0, "1D1=1D1X2=1X2=1D", 0);   
+    blonde::alignment::AlignmentType global = blonde::alignment::AlignmentType::kGlobal;
+    testAlignmentScenario("GCATGCU", "GATTACA", global, 0, "1=1D1=1I1=1X1=1X", 0);
+    testAlignmentScenario("AGTCTTTGAT", "GATTAGA", global, 0, "1D1=1D1X2=1X2=1D", 0);   
 }
 
 TEST(BlondeAlignmentTests, Local) {
-    testAlignmentScenario("AGTCTTTGAT", "GATTAGA", blonde::alignment::AlignmentType::kLocal, 3, "2S2=1X2=", 4);
-    testAlignmentScenario("AGTAGT", "TAG", blonde::alignment::AlignmentType::kLocal, 3, "3=", 2);
+    blonde::alignment::AlignmentType local = blonde::alignment::AlignmentType::kLocal;
+    testAlignmentScenario("AGTCTTTGAT", "GATTAGA", local, 3, "2S2=1X2=", 4);
+    testAlignmentScenario("AGTAGT", "TAGA", local, 3, "3=1S", 2);
+}
+
+TEST(BlondeAlignmentTests, SemiGlobal) {
+    blonde::alignment::AlignmentType semi = blonde::alignment::AlignmentType::kSemiGlobal;
+    testAlignmentScenario("AGTCTTTGAT", "GATTAGA", semi, 3, "3=4I", 7);
+    testAlignmentScenario("AGTAGT", "TAGA", semi, 2, "3=1D1I", 2);
 }
