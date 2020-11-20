47183a32e6c06935797562f89e254c55c27854d5
Leonard.Inkret@avl.com <Leonard.Inkret@avl.com>
2020-11-20 10:31:49 +0100

    add new global alignment test

--- test/blonde_alignment_test.cpp
+++ test/blonde_alignment_test.cpp
@@ -1,9 +1,9 @@
-#include "../src/blonde_alignment.h"
 #include <gtest/gtest.h>
+#include "../src/blonde_alignment.h"
 
-TEST(BlondeAlignmentTests, Global) {
-    const char* query = "GATTACA";
+TEST(BlondeAlignmentTests, Global_1) {
     const char* target = "GCATGCU";
+    const char* query = "GATTACA";
 
     std::string cigar;
     unsigned int target_begin;
@@ -11,5 +11,18 @@ TEST(BlondeAlignmentTests, Global) {
     int value = blonde::alignment::Align(query, strlen(query), target, strlen(target), blonde::alignment::AlignmentType::kGlobal, 1, -1, -1, &cigar, &target_begin);
     EXPECT_EQ(value, 0);
     EXPECT_EQ(cigar, "1=1D1=1I1=1X1=1X");
+    EXPECT_EQ(target_begin, 0);
+}
+
+TEST(BlondeAlignmentTests, Global_2) {
+    const char* target = "AGTCTTTGAT";
+    const char* query = "GATTAGA";
+
+    std::string cigar;
+    unsigned int target_begin;
+
+    int value = blonde::alignment::Align(query, strlen(query), target, strlen(target), blonde::alignment::AlignmentType::kGlobal, 1, -1, -1, &cigar, &target_begin);
+    EXPECT_EQ(value, 0);
+    EXPECT_EQ(cigar, "1D1=1D1X2=1X2=1D");
     EXPECT_EQ(target_begin, 0);
 }
