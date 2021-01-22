6c577037851703e21111a81f74255916200dbd53
MarijaOrec <marija.orec@fer.hr>
2021-01-22 21:39:49 +0100

    test 2

--- test/white_minimizers_test.cpp
+++ test/white_minimizers_test.cpp
@@ -8,9 +8,9 @@ TEST(WhiteMinimizersTests, First) {
     std::vector<std::tuple<unsigned int, unsigned int, bool>> result = white::Minimize(seq, 6, 3, 3);
     
     std::vector<std::tuple<unsigned int, unsigned int, bool>> expected = {
-    std::make_tuple(5,1,0),
+    std::make_tuple(19,4,0),
     std::make_tuple(14,0,1),
-    std::make_tuple(27,3,0)
+    std::make_tuple(36,3,1)
     };
 
     EXPECT_EQ(result,expected);
@@ -24,10 +24,13 @@ TEST(WhiteMinimizersTests, Second) {
     std::vector<std::tuple<unsigned int, unsigned int, bool>> result = white::Minimize(seq, 11, 3, 3);
     
     std::vector<std::tuple<unsigned int, unsigned int, bool>> expected = {
-    std::make_tuple(6,3,0),
-    std::make_tuple(6,8,0),
+	std::make_tuple(14,7,1),
+    std::make_tuple(17,1,1),
+    std::make_tuple(14,2,1),
+    std::make_tuple(36,0,1),
     std::make_tuple(16,5,1),
-    std::make_tuple(27,0,0)
+    std::make_tuple(57,8,1),
+    std::make_tuple(3,6,1)
     };
 
     EXPECT_EQ(result,expected);
